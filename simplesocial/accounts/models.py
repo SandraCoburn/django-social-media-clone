642a3d528f5ce6b792872b9265cf59cd0986aa11
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-07-26 15:16:23 -0700

    Created accounts models, login and signup html files. Created Usersmodel

--- simplesocial/accounts/models.py
+++ simplesocial/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib import auth
 
 # Create your models here.
+class User(auth.models.User,auth.models.PermissionsMixin):
+
+  def __str__(self) -> str:
+      #auth.models.User will bring a form with username etc
+      return "@{}".format(self.username)
