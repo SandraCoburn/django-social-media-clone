b809a7cf68db3bd37ad6bb4c10e61bbe452a9c53
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-07-27 15:05:14 -0700

    Added urls for test and thanks pages

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -122,6 +122,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIR = [Path(BASE_DIR)/'static']
 
+LOGIN_REDIRECT_URL = 'tests'
+LOGOUT_REDIRECT_URL = 'thanks'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
