ec184dae374185c133c85af4a3644c98cd02eff5
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-08-02 12:42:59 -0700

    Added group views and urls

--- simplesocial/groups/urls.py
+++ simplesocial/groups/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
   path('', views.ListGroups.as_view(), name='all'),
   path("new/", views.CreateGroup.as_view(),name='create'),
   path('posts/in/<slug>/', views.SingleGroup.as_view(), name='single'),
+  path('join/', views.JoinGroup.as_view(), name='join'),
+  path('leave/', views.LeaveGroup.as_view(), name='leave')
  
 ]
